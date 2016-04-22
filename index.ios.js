f8fca3d2992becd43f064ecab16d43afa7fc31ad
Vladimir Enchev <vladimir.enchev@telerik.com>
2016-04-22 16:28:32 +0300

    ios code updated

--- index.ios.js
+++ index.ios.js
@@ -6,7 +6,7 @@ exports.show = function (options) {
     return new Promise(function (resolve, reject) {
         try {
             if (options) {
-                var alert = SDCAlertController.alloc().initWithTitleMessagePreferredStyle(options.title || "", options.message || "", AlertControllerStyle.Alert);
+                var alert = SDCAlertController.alloc().initWithTitleMessagePreferredStyle(options.title || "", options.message || "", SDCAlertControllerStyle.Alert);
 
                 if (options.nativeView instanceof UIView) {
                     alert.contentView.addSubview(options.nativeView);
@@ -35,21 +35,21 @@ exports.show = function (options) {
 
                 if (options.cancelButtonText) {
                     alert.addAction(SDCAlertAction.alloc().initWithTitleStyleHandler(options.cancelButtonText,
-                        AlertActionStyle.Default, function (args) {
+                        SDCAlertActionStyle.Default, function (args) {
                             resolve(false);
                         }));
                 }
 
                 if (options.neutralButtonText) {
                     alert.addAction(SDCAlertAction.alloc().initWithTitleStyleHandler(options.neutralButtonText,
-                        AlertActionStyle.Default, function (args) {
+                        SDCAlertActionStyle.Default, function (args) {
                             resolve(undefined);
                         }));
                 }
 
                 if (options.okButtonText) {
                     alert.addAction(SDCAlertAction.alloc().initWithTitleStyleHandler(options.okButtonText,
-                        AlertActionStyle.Default, function (args) {
+                        SDCAlertActionStyle.Default, function (args) {
                             resolve(true);
                         }));
                 }
